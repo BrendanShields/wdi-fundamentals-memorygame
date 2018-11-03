bde698e142ee7f1cd7d45d49c26acc466fd44592
Brendan Shields <brendan.m.j.shields@gmail.com>
2018-11-04 01:17:31 +1100

    Made code more DRY

--- memory_game/all_memory_game_files/js/main.js
+++ memory_game/all_memory_game_files/js/main.js
@@ -1,29 +1,21 @@
 
 // Global Variables for card game;
-
 let cards 		= ['queenOfDiamonds','queenOfHearts','kingOfDiamonds','kingOfHearts']
 let cardsInPlay = [];
 
-// User Choice;
+// checks for match, returns result;
+function checkForMatch () {
+if (cardsInPlay[0] === cardsInPlay[1]) {
+console.log("You found a match!");
+}else{console.log("Sorry, try again.");}}
 
-var cardOne 	= cards[0]
-var cardTwo 	= cards[1]
 
-// Push result of User Choice into CardsInPlay Array;
+// Function logging cards flipped, pushing result of user input & running checkMatch
+var flipCard = function(cardId) {
+console.log(`You flipped ${cards[cardId]}`);
+cardsInPlay.push(cards[cardId]);
+checkForMatch()}
 
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-
-//Log to console User Choice
-
-console.log('You flipped ' + cardsInPlay[0] + ' and ' + cardsInPlay[1]);
-
-// Checks whether user has chosen the two cards, if they have, and they are equal return win else lose;
-
-if (cardsInPlay.length === 2) {
-if (cardsInPlay[0] 	   === cardsInPlay[1]) {
-		alert('You Win')
-} else {
-		alert('You Lose')
-}
-}
+//userInput
+flipCard(1)
+flipCard(1)
