cd74c460973bda5c16c5d658f8cd80e87362c4d4
Brendan Shields <brendan.m.j.shields@gmail.com>
2018-11-04 00:36:58 +1100

    memory game logic

--- memory_game/all_memory_game_files/js/main.js
+++ memory_game/all_memory_game_files/js/main.js
@@ -1,6 +1,29 @@
-let cardOne = 'queenOfDiamonds';
-let cardTwo = 'queenOfHearts'
-let cardThree = 'kingOfDiamonds'
-let cardFour = 'kingOfHearts'
 
-console.log('You flipped' + cardOne);
+// Global Variables for card game;
+
+let cards 		= ['queenOfDiamonds','queenOfHearts','kingOfDiamonds','kingOfHearts']
+let cardsInPlay = [];
+
+// User Choice;
+
+var cardOne 	= cards[0]
+var cardTwo 	= cards[1]
+
+// Push result of User Choice into CardsInPlay Array;
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+//Log to console User Choice
+
+console.log('You flipped ' + cardsInPlay[0] + ' and ' + cardsInPlay[1]);
+
+// Checks whether user has chosen the two cards, if they have, and they are equal return win else lose;
+
+if (cardsInPlay.length === 2) {
+if (cardsInPlay[0] 	   === cardsInPlay[1]) {
+		alert('You Win')
+} else {
+		alert('You Lose')
+}
+}
