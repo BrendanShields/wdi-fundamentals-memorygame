73d7f64839fd8b418c9add7c5cbed5cb5539d940
Brendan Shields <brendan.m.j.shields@gmail.com>
2018-11-08 12:51:15 +1100

    deliverable memory game

--- memory_game/all_memory_game_files/js/main.js
+++ memory_game/all_memory_game_files/js/main.js
@@ -1,21 +1,20 @@
-
 // Global Variables for card game;
 let cards 		= 
-[{rank: 'queen',
- suit: 'hearts',
- cardImage: "images/queen-of-hearts.png"},
+	[{rank: 'queen',
+	 suit: 'hearts',
+	 cardImage: "all_memory_game_files/images/queen-of-hearts.png"},
 
- {rank: 'queen',
- suit: 'diamonds',
- cardImage: "images/queen-of-diamonds.png"},
+	 {rank: 'queen',
+	 suit: 'diamonds',
+	 cardImage: "all_memory_game_files/images/queen-of-diamonds.png"},
 
- {rank: 'king',
- suit: 'hearts',
- cardImage: "images/king-of-hearts.png"},
+	 {rank: 'king',
+	 suit: 'hearts',
+	 cardImage: "all_memory_game_files/images/king-of-hearts.png"},
 
- {rank: 'king',
- suit: 'diamonds',
- cardImage: "images/king-of-diamonds.png"}]
+	 {rank: 'king',
+	 suit: 'diamonds',
+	 cardImage: "all_memory_game_files/images/king-of-diamonds.png"}]
 
 
 
@@ -23,21 +22,34 @@ let cardsInPlay = [];
 
 // checks for match, returns result;
 function checkForMatch () {
-if (cardsInPlay[0] === cardsInPlay[1]) {
-console.log("You found a match!");
-alert('You Win!')
-}else{console.log("Sorry, try again."); 
-	  alert('You lose!')}}
-
-
-// Function logging cards flipped, pushing result of user input & running checkMatch
-var flipCard = function(cardId) {
-console.log(`You flipped the ${cards[cardId].rank} of ${cards[cardId].suit} ${cards[cardId].cardImage}`);
-cardsInPlay.push(cards[cardId].rank);
-if (cardsInPlay.length == 2) 
-	{checkForMatch()}
+	var divResult = document.getElementById('result');
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+	console.log("You found a match!");
+	alert('You Win!')
+	}else{console.log("Sorry, try again."); 
+	alert('You lose!')
+	}
 }
 
-//userInput
-flipCard(2)
-flipCard(1)
+function flipCard() {
+    var cardId = this.getAttribute('data-id');
+  cardsInPlay.push(cards[cardId].rank);
+ this.setAttribute('src', cards[cardId].cardImage);
+ if (cardsInPlay.length === 2){
+    checkForMatch();
+	};
+};
+
+function createBoard () {
+for (var i = 0; i < cards.length; i++) {
+var cardElement = document.createElement("img");      
+	cardElement.setAttribute('src', 'all_memory_game_files/images/back.png');
+	cardElement.setAttribute('data-id', i)
+	cardElement.addEventListener('click', flipCard);
+	document.getElementById('game-board').appendChild(cardElement);
+
+}
+	}
+
+
+createBoard()
