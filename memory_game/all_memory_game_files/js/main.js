ab650b1c8f0efeece8aec785ce129cb51d9ddbeb
Brendan Shields <brendan.m.j.shields@gmail.com>
2018-11-09 19:46:48 +1100

    final iteration

--- memory_game/all_memory_game_files/js/main.js
+++ memory_game/all_memory_game_files/js/main.js
@@ -1,74 +1,109 @@
 // Global Variables for card game;
-let cards 		= 
-	[{rank: 'queen',
-	 suit: 'hearts',
-	 cardImage: "all_memory_game_files/images/queen-of-hearts.png"},
+	
+	let cards 		= 
+		[{rank: 'queen',
+		 suit: 'hearts',
+		 cardImage: "all_memory_game_files/images/queen-of-hearts.png"},
 
-	 {rank: 'queen',
-	 suit: 'diamonds',
-	 cardImage: "all_memory_game_files/images/queen-of-diamonds.png"},
+		 {rank: 'queen',
+		 suit: 'diamonds',
+		 cardImage: "all_memory_game_files/images/queen-of-diamonds.png"},
 
-	 {rank: 'king',
-	 suit: 'hearts',
-	 cardImage: "all_memory_game_files/images/king-of-hearts.png"},
+		 {rank: 'king',
+		 suit: 'hearts',
+		 cardImage: "all_memory_game_files/images/king-of-hearts.png"},
 
-	 {rank: 'king',
-	 suit: 'diamonds',
-	 cardImage: "all_memory_game_files/images/king-of-diamonds.png"}]
+		 {rank: 'king',
+		 suit: 'diamonds',
+		 cardImage: "all_memory_game_files/images/king-of-diamonds.png"}]
+
+
+// shuffle: while elements in array pick random index & decrease ctr by1
+// then swap the last element with it
 
 
 function shuffle(arra1) {
-    var ctr = arra1.length, temp, index;
-
-// While there are elements in the array
-    while (ctr > 0) {
-// Pick a random index
-        index = Math.floor(Math.random() * ctr);
-// Decrease ctr by 1
-        ctr--;
-// And swap the last element with it
-        temp = arra1[ctr];
-        arra1[ctr] = arra1[index];
-        arra1[index] = temp;
-    }
-    return arra1;
+	    var ctr = arra1.length, temp, index;
+	while (ctr > 0) {
+	    index = Math.floor(Math.random() * ctr);
+	    ctr--;
+	    temp = arra1[ctr];
+	    arra1[ctr] = arra1[index];
+	    arra1[index] = temp;
+	    }
+  return arra1;
 }
+
 cards = shuffle(cards);
 
 
-let cardsInPlay = [];
 
-// checks for match, returns result;
+//sets cards in play checks for match, returns result.
+
+	let cardsInPlay = [];
+
 function checkForMatch () {
-	var divResult = document.getElementById('result');
+		var divResult = document.getElementById('result');
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-	var content = document.createTextNode("You Win!!");
-	divResult.appendChild(content);
+		var content = document.createTextNode("You Win!!");
+		divResult = document.getElementById('result').textContent = 'You Win!!';
+		divResult.appendChild(content);
 	}else{
-	var content = document.createTextNode("You lose!!");
-	divResult.appendChild(content);
+		var content = document.createTextNode("Better Luck Next Time!!");
+		divResult = document.getElementById('result').textContent = 'You Lose!!'
+		divResult.appendChild(content);
 	}
 }
 
+
+//takes the card data from createBoard & if 2 cards 
+//flipped starts checkmatch function
+
 function flipCard() {
-    var cardId = this.getAttribute('data-id');
-  cardsInPlay.push(cards[cardId].rank);
- this.setAttribute('src', cards[cardId].cardImage);
- if (cardsInPlay.length === 2){
-    checkForMatch();
+    	var cardId = this.getAttribute('data-id');
+  		cardsInPlay.push(cards[cardId].rank);
+ 		this.setAttribute('src', cards[cardId].cardImage);
+ 	if (cardsInPlay.length === 2){
+    	checkForMatch();
 	};
 };
 
+
+//creates an img element, passes the image data from the cards object
+//then applies event listener to being flipcard function on click and
+//appends the card to the game-board div
+
 function createBoard () {
-for (var i = 0; i < cards.length; i++) {
-var cardElement = document.createElement("img");      
-	cardElement.setAttribute('src', 'all_memory_game_files/images/back.png');
-	cardElement.setAttribute('data-id', i)
-	cardElement.addEventListener('click', flipCard);
-	document.getElementById('game-board').appendChild(cardElement);
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement("img");      
+		cardElement.setAttribute('src', 'all_memory_game_files/images/back.png');
+		cardElement.setAttribute('data-id', i)
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
 
 }
 	}
 
+createBoard()	
 
-createBoard()
+
+//sets event listener for button.
+
+		document.querySelector('button').addEventListener('click', reset)
+
+//loops through the length of cards array finding the img nodes and
+//removing them, shuffles the cards array, creates a new board and resets
+//the value for cards in play before setting the result text to 'try again'
+
+
+function reset () {
+	for (i = 0; i < cards.length; i++) {
+		var game = document.querySelector('img');
+		game.parentNode.removeChild(game);
+ 	}
+
+		cards = shuffle(cards)
+		createBoard()
+		cardsInPlay = [];
+	divResult = document.getElementById('result').textContent = 'Try Again!'
+}
